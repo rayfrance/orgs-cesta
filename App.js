ded6b12d3e00ae95d80b4247af79d1cd9b9c275d
rayfrance <rayanethaisglfrance@gmail.com>
2023-03-16 14:38:05 -0300

    refatorando a aplicação com componentes estilizados

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar , SafeAreaView , StyleSheet, Text, View } from 'react-native';
-import Cesta from './src/telas/cesta.js';
+import Cesta from './src/telas/Cesta/index.js';
 import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 
 export default function App() {
